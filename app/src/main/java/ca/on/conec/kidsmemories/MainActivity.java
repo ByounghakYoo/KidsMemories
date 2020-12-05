5c990bef6a6cae6f2620b4e9324675ebf7666ee0
Byounghak <ybhsb1@gmail.com>
2020-12-05 10:33:23 -0500

    Initial Commit;

--- app/src/main/java/ca/on/conec/kidsmemories/MainActivity.java
+++ app/src/main/java/ca/on/conec/kidsmemories/MainActivity.java
@@ -3,18 +3,16 @@ package ca.on.conec.kidsmemories;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
     Intent intent;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
